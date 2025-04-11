5e2a7afd581ba78505a0dac207539336e9945ffa
Nicki tst <kanieleoutiz@gmail.com>
2025-04-11 11:47:02 +0300

    add gather_fields

--- async_requests.py
+++ async_requests.py
@@ -1,10 +1,17 @@
+import asyncio
+
 import aiohhtp
 
 from models import SwapiPeople, Session, init_orm, close_orm
 
 MAX_CONCURRENT_REQUESTS = 5
 
-async def fetch_all_swapi_people(url: str, session: aiohhtp.ClientSession, key: str = "name") -> str:
+async def fetch_data(url: str, session: aiohhtp.ClientSession, key: str = "name") -> str:
+    """
+    Выполняет GET запрос к указанному URL и возвращает значение:
+    для фильмов - title
+    для персонажей - name
+    """
     try:
         async with session.get(url) as response:
             if response.status != 200:
@@ -15,5 +22,10 @@ async def fetch_all_swapi_people(url: str, session: aiohhtp.ClientSession, key: 
         print(f"Ошибка при запросе {url}: {e}")
         return ""
 
-
+async def gather_fields(urls: list, session: aiohhtp.ClientSession, key: str = "name") -> str:
+    """
+    Выполняет запросы для списка URL и возвращает строку, разделенную через запятую
+    """
+    results = await asyncio.gather(*[fetch_data(url, session, key) for url in urls])
+    return ", ".join(filter(None, results))
 
