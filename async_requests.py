79b76c697456b84fb0ab9f53e90fb41e09bd1b31
Nicki tst <kanieleoutiz@gmail.com>
2025-04-11 11:57:55 +0300

    add get_person

--- async_requests.py
+++ async_requests.py
@@ -29,3 +29,45 @@ async def gather_fields(urls: list, session: aiohhtp.ClientSession, key: str = "
     results = await asyncio.gather(*[fetch_data(url, session, key) for url in urls])
     return ", ".join(filter(None, results))
 
+async def get_person(person_id: int, session: aiohhtp.ClientSession) -> dict:
+    """
+    Получает
+    """
+    url = f"https://swapi.dev/api/people/{person_id}/"
+    try:
+        async with session.get(url) as response:
+            if response.status != 200:
+                print(f"Персонаж с ID {person_id} не найден")
+                return None
+            data = await response.json()
+    except Exception as e:
+        print(f"Ошибка при получении персонажа с ID {person_id}: {e}")
+        return None
+
+    person = {
+        "id": person_id,
+        "birth_year": data.get("birth_year", ""),
+        "eye_color": data.get("eye_color", ""),
+        "gender": data.get("gender", ""),
+        "hair_color": data.get("hair_color", ""),
+        "height": data.get("height", ""),
+        "mass": data.get("mass", ""),
+        "name": data.get("name", ""),
+        "skin_color": data.get("skin_color", "")
+    }
+
+    homeworld = data.get("homeworld")
+    if homeworld:
+        person["homeworld"] = await fetch_data(homeworld, session, key="name")
+    else:
+        person["homeworld"] = ""
+
+
+    # Обработка списков вложенных значений
+    person["films"] = await gather_fields(data.get("films", []), session, key="title") if data.get("films") else ""
+    person["species"] = await gather_fields(data.get("species", []), session, key="name") if data.get("species") else ""
+    person["starships"] = await gather_fields(data.get("starships", []), session, key="name") if data.get("starships") else ""
+    person["vehicles"] = await gather_fields(data.get("vehicles", []), session, key="name") if data.get("vehicles") else ""
+
+    return person
+
